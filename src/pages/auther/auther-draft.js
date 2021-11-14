d66da4c2c21acd7a0efff0f096fabda9be99ccbc
KeisukeYamane <johndoe@example.com>
2021-11-14 10:13:16 +0900

    name修正

--- src/pages/auther/auther-draft.js
+++ src/pages/auther/auther-draft.js
@@ -4,18 +4,21 @@ import queryString from "query-string"
 import AutherPost from "../../templates/auther-post"
 
 const AutherDraftPage = ({ location }) => {
-  const { contentId, draftKey } = queryString.parse(location.search);
+  const { contentId, draftKey, apiName } = queryString.parse(location.search);
   const [data, setData] = useState();
+  const queryName = `microcms${setHeadCharToUppercamel(apiName)}`
 
   console.log("contentId  " + contentId)
   console.log("draftKey  " + draftKey)
+  console.log("apiName  " + apiName)
   console.log(queryString.parse(location.search))
+  console.log("queryName  " + queryName)
   console.log("process.env.GATSBY_MICROCMS_API_KEY  " + process.env.GATSBY_MICROCMS_API_KEY)
 
   useEffect(() => {
-    // fetch(`https://simple-site.microcms.io/api/v1/authers/qznj0mhnlv?draftKey=gxyXbSS06A`,
+    fetch(`https://simple-site.microcms.io/api/v1/authers/qznj0mhnlv?draftKey=gxyXbSS06A`,
 
-    fetch(`https://simple-site.microcms.io/api/v1/authers/${contentId}?draftKey=${draftKey}`,
+    // fetch(`https://simple-site.microcms.io/api/v1/${apiName}/${contentId}?draftKey=${draftKey}`,
     {
       headers: {
         'X-MICROCMS-API-KEY' : "3db31925-660a-4bf1-b3d0-040fbf30f164",
@@ -37,3 +40,7 @@ const AutherDraftPage = ({ location }) => {
 }
 
 export default AutherDraftPage
+
+function setHeadCharToUppercamel(apiName) {
+    return apiName.charAt(0).toUpperCase() + apiName.slice(1)
+}
